b39cce49c874a2a2940474e1ed6314036b054503
ashraf <ashraf@gmail.com>
2025-12-11 18:33:15 +0530

    order service is started implementing

---
+++ src/main/java/com/nomaniskabab/orderservice/NomaniskababOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nomaniskabab.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomaniskababOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomaniskababOrderServiceApplication.class, args);
+	}
+
+}
